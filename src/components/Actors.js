4eb7459b952113ebdd598c0a0e6935773564708b
Kelvin Kuria <kkuria383@yahoo.com>
2024-02-18 08:01:25 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
-import React from "react";
-import { actors } from "../data";
+
+// Actors.js
+import React from 'react';
+import data from '../data';
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {data.actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,25 @@
-import React from "react";
-import { movies } from "../data";
+// Movies.js
+import React from 'react';
+import data from '../data';
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {data.movies.map((movie, index) => (
+        <div key={index}>
+          <h2>{movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
+
